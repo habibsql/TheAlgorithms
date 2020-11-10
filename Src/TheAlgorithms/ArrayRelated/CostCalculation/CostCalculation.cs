51a6d02f904ce5d1d7456b30c6377735e16c2bcb
habibsql <habibsql@gmail.com>
2020-11-10 20:15:53 +0600

    added new algorithm

---
+++ Src/TheAlgorithms/ArrayRelated/CostCalculation/CostCalculation.cs
@@ -0,0 +1,49 @@
+﻿namespace TheAlgorithms.ArrayRelated.CostCalculation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CostCalculation
+    {
+        public int CalculateMinimumForLastElement(int[,] input)
+        {
+            int n = 4;
+            var costList = new List<int>(1);
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentCost = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+                    currentCost += input[i, j];
+                }
+                int c = 3;
+                for (int index = i + 1; index < n; index++)
+                {
+                    int val = input[index, c];
+                    currentCost += val;
+                }
+
+                if (costList.Count == 0)
+                {
+                    costList.Add(currentCost);
+                }
+                else
+                {
+                    int previousCost = costList[0];
+
+                    if (previousCost > currentCost)
+                    {
+                        costList[0] = currentCost;
+                    }
+                }
+            }
+
+            return costList[0];
+        }
+    }
+
+
+}
---
+++ Src/TheAlgorithms/ArrayRelated/CostCalculation/CostCalculationTest.cs
@@ -0,0 +1,42 @@
+﻿namespace TheAlgorithms.ArrayRelated.CostCalculation
+{
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Xunit;
+
+    public class CostCalculationTest
+    {
+        private readonly CostCalculation costCalculation = new CostCalculation();
+
+        [Fact]
+        public void Should_Calulate_Minimum_Cost()
+        {
+            var input = new int[4, 4];
+            input[0, 0] = 2;
+            input[0, 1] = 4;
+            input[0, 2] = 3;
+            input[0, 3] = 7;
+
+            input[1, 0] = 6;
+            input[1, 1] = 5;
+            input[1, 2] = 1;
+            input[1, 3] = 3;
+
+            input[2, 0] = 9;
+            input[2, 1] = 6;
+            input[2, 2] = 2;
+            input[2, 3] = 8;
+
+            input[3, 0] = 1;
+            input[3, 1] = 3;
+            input[3, 2] = 4;
+            input[3, 3] = 9;
+
+            int minimumCost = costCalculation.CalculateMinimumForLastElement(input);
+
+            minimumCost.Should().Be(17);
+        }
+    }
+}
